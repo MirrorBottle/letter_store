d577a8914d98a12b5ee4c2ae87420702ab21fdb2
MirrorBottle <setiawanbayu66152@gmail.com>
2020-03-03 13:38:23 +0800

    Mengubah tampilan create

--- resources/views/mails/create.blade.php
+++ resources/views/mails/create.blade.php
@@ -43,42 +43,39 @@
                                         </span>
                                     @endif
                                 </div>
-                                <div class="form-group{{ $errors->has('docx') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-docx">{{ __('File (.docx)') }}</label>
-                                    <input type="text" name="docx" id="input-docx" class="form-control form-control-alternative{{ $errors->has('docx') ? ' is-invalid' : '' }}" placeholder="{{ __('File (.docx)') }}" value="{{ old('docx') }}" required>
-
-                                    @if ($errors->has('docx'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('docx') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-
-                                <div class="form-group{{ $errors->has('pdf') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-pdf">{{ __('File (.pdf)') }}</label>
-                                    <input type="text" name="pdf" id="input-pdf" class="form-control form-control-alternative{{ $errors->has('pdf') ? ' is-invalid' : '' }}" placeholder="{{ __('File (.pdf') }}" value="{{ old('pdf') }}" required>
-
-                                    @if ($errors->has('pdf'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('pdf') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-
-                                <div class="form-group{{ $errors->has('html') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-html">{{ __('File (.html)') }}</label>
-                                    <input type="text" name="html" id="input-html" class="form-control form-control-alternative{{ $errors->has('html') ? ' is-invalid' : '' }}" placeholder="{{ __('File (.html') }}" value="{{ old('html') }}" required>
-
-                                    @if ($errors->has('html'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('html') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-
-                                <div class="form-group{{ $errors->has('user') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-user">{{ __('') }}</label>
-                                    <input type="hidden" name="user" id="input-user" class="form-control form-control-alternative{{ $errors->has('user') ? ' is-invalid' : '' }}" placeholder="{{ __('User') }}" value="{{ old('user') }}" required>
+                                <div class="row">
+                                    <div class="col-md-6 col-12">
+                                        <label class="form-control-label" for="input-subject">{{ __('File Docx') }}</label>
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text bg-primary text-white" id="basic-addon1">
+                                                    <i class="fas fa-file-word"></i>
+                                                </span>
+                                            </div>
+                                            <input type="file" name="docx" class="form-control bg-primary text-white file-input" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                                        </div>
+                                        @if ($errors->has('docx'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('docx') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="col-md-6 col-12">
+                                        <label class="form-control-label" for="input-subject">{{ __('File PDF') }}</label>
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text bg-danger text-white" id="basic-addon1">
+                                                    <i class="fas fa-file-pdf"></i>
+                                                </span>
+                                            </div>
+                                            <input type="file" name="pdf" class="form-control bg-danger text-white file-input" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                                        </div>
+                                        @if ($errors->has('pdf'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('pdf') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
                                 </div>
 
                                 <div class="text-center">
@@ -94,3 +91,42 @@
         @include('layouts.footers.auth')
     </div>
 @endsection
+@push('css')
+    <style>
+        .upload-btn-wrapper {
+        position: relative;
+        overflow: hidden;
+        display: inline-block;
+        width: 100%;
+        }
+
+        .upload-btn-wrapper .btn {
+            color: #fff;
+            background-color: white;
+            padding: 8px 20px;
+            border-radius: 10px;
+            font-size: 20px;
+            font-weight: bold;
+        }
+        .upload-btn-wrapper .btn.btn-danger {
+            background: #F5365C;
+        }
+        .upload-btn-wrapper .btn.btn-primary {
+            background: #5E72E4;
+        }
+        .upload-btn-wrapper input[type=file] {
+        font-size: 100px;
+        position: absolute;
+        left: 0;
+        top: 0;
+        opacity: 0;
+        }
+    </style>
+@endpush
+@push('js')
+    <script>
+        function disabled() {
+            console.log($(this));
+        }
+    </script>
+@endpush
